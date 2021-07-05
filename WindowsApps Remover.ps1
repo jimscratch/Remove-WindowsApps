fdf4e889f86ac4b6a650059a1fdbaf90ca3bbc8e
Zezypisa <49347779+Zezypisa@users.noreply.github.com>
2021-07-04 21:28:32 -0400

    updated stuff

--- WindowsApps Remover.ps1
+++ WindowsApps Remover.ps1
@@ -6,45 +6,40 @@ Write-Host "Use this at your own risk."
 Write-Host "Note: Disable Delivery Optimization before using this in settings > update and security > delivery optimization."
 $confirmation = Read-Host "Are you Sure You Want To Proceed? [yes\no]"
 if ($confirmation -eq "yes"){
-    $drive = Read-Host "Please type the drive you would like to remove or takeown WindowsApps from"
-    $confirmation = Read-Host "You have selected to takeown"$drive":\, Are you sure you want to do this?"
+    $drive = Read-Host "Please type the drive you would like to remove or takeown WindowsApps from. [drive letter]"
+    $remove = Read-Host "Would you like to takeown or remove the WindowsApps folder. [takeown\remove]"
+    $confirmation = Read-Host "You have selected to"$remove $drive":\, Are you sure you want to do continue? [yes\no]"
     if ($confirmation -eq "yes"){
         #Takeown DeliveryOptimization
         takeown /f $drive":\DeliveryOptimization" /a /r /d y
         icacls $drive":\DeliveryOptimization" /grant administrators:F /T
-
+        
         #Takeown WindowsApps
         takeown /f $drive":\WindowsApps" /a /r /d y
         icacls $drive":\WindowsApps" /grant administrators:F /T
-
+        
         #Takeown WindowsApps
         takeown /f $drive":\Program Files\ModifiableWindowsApps" /a /r /d y
         icacls $drive":\Program Files\ModifiableWindowsApps" /grant administrators:F /T
-
+        
         #Takeown WpSystem
         takeown /f $drive":\WpSystem" /a /r /d y
         icacls $drive":\WpSystem" /grant administrators:F /T
-
+        
         #Takeown WUDownloadCache
         takeown /f $drive":\WUDownloadCache" /a /r /d y
         icacls $drive":\WUDownloadCache" /grant administrators:F /T
 
-        $confirmation = Read-Host "Would you now like to remove WindowsApps from"$drive":\? [yes/no]"
-        if ($confirmation -eq "yes"){
+        if ($remove -eq "remove"){
             Remove-Item $drive":\DeliveryOptimization" -Force -Recurse
             Remove-Item $drive":\WindowsApps" -Force -Recurse
             Remove-Item $drive":\Program Files\ModifiableWindowsApps" -Force -Recurse
             Remove-Item $drive":\WpSystem" -Force -Recurse
             Remove-Item $drive":\WUDownloadCache" -Force -Recurse
-            Write-Host "Finished."
-            pause
-            exit
         }
-        if ($confirmation -eq "no"){
-            Write-Host "Alright, Stopped. No files have been removed"
-            pause
-            exit
-        }
+        Write-Host "Finished."
+        pause
+        exit
     }
     if ($confirmation -eq "no"){
         Write-Host "Alright, Cancelled. No files have been modified"
